79a34e324bd41be7907284e00b743115f2964fb9
MhmdAbed11 <mohammad_aabed@hotmail.com>
2019-12-13 23:50:02 +0200

    add sendgrid email service

--- src/graphql/user/resolvers/user.mutation.js
+++ src/graphql/user/resolvers/user.mutation.js
@@ -1,6 +1,38 @@
 import jwt from "jsonwebtoken";
 import bcrypt from "bcryptjs";
 import { APP_SECRET, getUserId } from "../../../utils";
+const sgMail = require("@sendgrid/mail");
+const MailGen = require("mailgen");
+const SENDGRID_API_KEY =
+  "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";
+sgMail.setApiKey(SENDGRID_API_KEY);
+
+const mailGenerator = new MailGen({
+  theme: "salted",
+  product: {
+    name: "Awesome App",
+    link: "http://example.com"
+    // logo: your app logo url
+  }
+});
+
+const email = {
+  body: {
+    name: "Jon Doe",
+    intro: "Welcome to email verification",
+    action: {
+      instructions: "Please click the button below to verify your account",
+      button: {
+        color: "#33b5e5",
+        text: "Verify account",
+        link: "http://example.com/verify_account"
+      }
+    }
+  }
+};
+
+const emailTemplate = mailGenerator.generate(email);
+require("fs").writeFileSync("preview.html", emailTemplate, "utf8");
 
 const Mutation = {
   signup: async (root, args, context, info) => {
@@ -15,7 +47,14 @@ const Mutation = {
       password
     });
     const token = jwt.sign({ userId: user.id }, APP_SECRET);
-
+    const msg = {
+      to: args.email,
+      from: "mabed4297@gmail.com",
+      subject: "Sending with Twilio SendGrid is Fun",
+      text: "and easy to do anywhere, even with Node.js",
+      html: emailTemplate
+    };
+    sgMail.send(msg);
     return {
       token,
       user
