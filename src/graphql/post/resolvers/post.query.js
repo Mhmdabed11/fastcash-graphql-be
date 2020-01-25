3247d0e152be308bae3a861b4044659600cc9aa8
MhmdAbed11 <mohammad_aabed@hotmail.com>
2020-01-25 21:34:58 +0200

    add filtering when fetching posts

--- src/graphql/post/resolvers/post.query.js
+++ src/graphql/post/resolvers/post.query.js
@@ -5,10 +5,22 @@ const Query = {
     return post;
   },
   posts: async (root, args, context, info) => {
+    let partialFilter = {};
+    if (args.filter) {
+      partialFilter.title_contains = args.filter;
+    }
+    if (args.location) {
+      partialFilter.location = args.location;
+    }
+    if (args.category) {
+      partialFilter.category_contains = args.category;
+    }
+    const where = partialFilter;
     const posts = await context.prisma.posts({
       skip: args.skip,
       first: args.first,
-      last: args.last
+      last: args.last,
+      where
     });
     return posts;
   }
